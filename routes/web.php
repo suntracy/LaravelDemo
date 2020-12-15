efcb52a38e4ff650dd2c697b0c8c7e8cd940088d
suntracy <tracy881209@gmail.com>
2020-12-15 14:14:04 +0800

    12.15

--- routes/web.php
+++ routes/web.php
@@ -20,45 +20,63 @@ Route::get('/', function () {
 });
 
 Route::get('/hello-world', function () {
-
     return view('hello_world');
-
 }); 
 
 Route::get('/about_us', function () {
-
     return view('about_us',['name' => 'Laravel 範例']);
-
 });
 
 Route::get('/inspire', 'App\Http\Controllers\InspiringController@inspire'); 
 
 Route::get('/test', function(){
-
     return App\Models\Post::all();
-
 }); 
 
 Route::get('/edit', function(){
-
     $post = App\Models\Post::find(1);
-
     $post->content = 'Laravel demo';
-
     $post->save();
-
     return $post;
-
 }); 
 
 Route::get('/add', function(){
-
     $post = new App\Models\Post;
- 
      $post->content = 'abcde';
- 
      $post->save();
- 
      return $post; 
- 
  }); 
+
+
+ Route::get('/delete', function(){
+    $post = App\Models\Post::find(1);
+    $post->delete();
+}); 
+
+Route::get('/sub1', function(){
+    $posts = new App\Models\Subject;
+    $posts->name = 'computer';
+    $posts->save();
+    return $posts;
+}); 
+
+Route::get('/sub2', function(){
+    $posts = new App\Models\Subject;
+    $posts->name = 'network';
+    $posts->save();
+    return $posts;
+}); 
+
+Route::get('/login', function(){
+    var_dump(Auth::check());
+}); 
+
+Route::get('/infor', function(){
+    echo Auth::user();
+}); 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('posts', 'PostController');
